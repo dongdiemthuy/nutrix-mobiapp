6d463cd496fec8b12e28af16edcfe7cbe5ed1cfa
dongdiemthuy <dongdiemthuy@gmail.com>
2015-03-30 16:48:32 +0700

    change mobiapp from windown to ubuntu angularjs

---
+++ platforms/android/assets/www/js/app.js
@@ -0,0 +1,84 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+  .state('app', {
+    url: "/app",
+    abstract: true,
+    templateUrl: "templates/menu.html",
+    controller: 'AppCtrl'
+  })
+
+  // .state('app.createmenu', {
+  //     url: "/createmenu",
+  //     views: {
+  //       'menuContent': {
+  //         templateUrl: "templates/createmenu.html"
+  //       }
+  //     }
+  //   })
+
+  .state('app.ingredient', {
+    url: "/ingredient",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/ingredient.html",
+        controller: 'IngredientlistsCtrl'
+      }
+    }
+  })
+
+  .state('app.ingredientdetail', {
+    url: "/ingredient/:id",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/detailingredient.html",
+        controller: 'IngredientDetailCtrl'
+      }
+    }
+  })
+  
+
+  .state('app.nutrients', {
+      url: "/nutrientlists",
+      views: {
+        'menuContent': {
+          templateUrl: "templates/nutrient.html",
+          controller: 'NutrientlistsCtrl'
+        }
+      }
+    })
+
+  .state('app.single', {
+    url: "/nutrient/:id",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/detailnutrientlist.html",
+        controller: 'NutrientDetailCtrl'
+      }
+    }
+  });
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/nutrientlists');
+});
---
+++ www/js/controllers.js
@@ -0,0 +1,228 @@
+
+angular.module('starter.controllers', [])
+.constant('baseurl', 'http://192.168.0.149:7777')
+.controller('AppCtrl', function($scope, $ionicModal, $timeout, baseurl) {
+  // Form data for the login modal
+  $scope.loginData = {};
+  $scope.modal = [];
+
+  // $scope.registrationData = {};
+
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/login.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal['login'] = modal;
+  });
+
+  // Triggered in the login modal to close it
+  $scope.closeLogin = function() {
+    $scope.modal['login'].hide();
+  };
+
+  // Open the login modal
+  $scope.login = function() {
+    $scope.modal['login'].show();
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    console.log('Doing login', $scope.loginData);
+
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.closeLogin();
+    }, 1000);
+  };
+
+  $ionicModal.fromTemplateUrl('templates/registration.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal['registration'] = modal;
+  });
+
+  // Triggered in the login modal to close it
+  $scope.closeRegistration = function() {
+    $scope.modal['registration'].hide();
+  };
+
+  // Open the login modal
+  $scope.registration = function() {
+    $scope.modal['registration'].show();
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.doRegistration = function() {
+    console.log('Doing registration', $scope.registrationData);
+
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.closeRegistration();
+    }, 1000);
+  };
+})
+
+// .controller('NutrientlistsCtrl', function($scope, $http) {
+//   $scope.nutrients = [];
+//    $http.get('http://localhost:7777/nutrix-app/ws/nutrix/adm/nutrient').then(function(resp) {
+//     console.log('Success', resp);
+//     $scope.nutrients = resp.data.collection;
+//   }, function(err) {
+//     console.error('ERR', err);
+//   })
+// })
+
+.controller('NutrientlistsCtrl', function($scope, $http, baseurl) {
+  $scope.nutrients = [];
+  $scope.nutrientMeta = {};
+
+  $scope.nutrientMeta['start'] = 0;
+  $scope.nutrientMeta['limit'] = 150;
+  //$scope.nutrientMeta['range'] = 150;
+
+  $scope.loadMoreNutrients = function() {
+    $http.get(baseurl + '/nutrix-app/ws/nutrix/adm/nutrient', {
+      params: {
+         offset: $scope.nutrientMeta['start'],
+         max: $scope.nutrientMeta['limit']
+      }
+    }).then(function(resp) {
+        console.log('Success', resp);
+        $scope.nutrients = $scope.nutrients.concat(resp.data.collection);
+        $scope.nutrientMeta['start'] += resp.data.collection.length;
+        $scope.$broadcast('scroll.infiniteScrollComplete');
+      }, function(err) {
+        console.error('ERR', err);
+      });
+  };
+
+  $scope.$on('$stateChangeSuccess', function() {
+    //$scope.loadMoreIngredients();
+  });
+})
+
+.controller('NutrientDetailCtrl', function($scope, $http, $stateParams, baseurl) {
+
+    $scope.nutrient = [];
+    var id = $stateParams.id;
+    $http.get(baseurl + '/nutrix-app/ws/nutrix/adm/nutrient/' + id).then(function(resp) {
+    console.log('Success', resp);
+    $scope.nutrient = resp.data;
+    
+  }, function(err) {
+    console.error('ERR', err);
+  
+  })
+})
+
+.controller('IngredientlistsCtrl', function($scope, $http, nutrientList, baseurl) {
+  $scope.ingredients = [];
+  $scope.ingredientMeta = {};
+
+  $scope.init = function() {
+    $scope.ingredients = [];
+    $scope.ingredientMeta['total'] = -1;
+    $scope.ingredientMeta['start'] = 0;
+    $scope.ingredientMeta['limit'] = 30;
+    $scope.ingredientMeta['range'] = 50;
+    delete $scope.ingredientMeta['query'];
+  }
+
+  $scope.moreDataCanBeLoaded = function() {
+    console.log('aaaa');
+    if (($scope.ingredientMeta['total'] == -1) || 
+        ($scope.ingredientMeta['start'] < $scope.ingredientMeta['total'])) {
+      return true;
+    }
+    return false;
+  }
+
+  $scope.loadMoreIngredients = function() {
+    var myparams = {
+        offset: $scope.ingredientMeta['start'],
+        max: $scope.ingredientMeta['limit']
+    }
+
+    if ($scope.ingredientMeta['query'] != undefined && $scope.ingredientMeta['query'] != null) {
+      myparams['q'] = $scope.ingredientMeta['query'];
+    }
+
+    $http.get(baseurl + '/nutrix-app/ws/nutrix/adm/ingredient', {
+        params: myparams
+      }).then(function(resp) {
+          console.log('Success', resp);
+          $scope.ingredientMeta['total'] = resp.data.total;
+          $scope.ingredients = $scope.ingredients.concat(resp.data.collection);
+          $scope.ingredientMeta['start'] += resp.data.collection.length;
+          $scope.$broadcast('scroll.infiniteScrollComplete');
+        }, function(err) {
+          console.error('ERR', err);
+        });
+  };
+
+  $scope.$on('$stateChangeSuccess', function() {
+    $scope.loadMoreIngredients();
+  });
+
+  $scope.doSearchIngredients = function(q) {
+      console.log('doSearchIngredients');
+      $scope.init();
+      $scope.ingredientMeta['query'] = q;
+      $scope.loadMoreIngredients();
+  };
+
+  $scope.init();
+ 
+})
+
+
+.controller('IngredientDetailCtrl', function($scope, $http, $stateParams,nutrientList, baseurl) {
+
+    $scope.ingredient = [];
+    var id = $stateParams.id;
+    $http.get(baseurl + '/nutrix-app/ws/nutrix/adm/ingredient/' + id).then(function(resp) {
+    console.log('Success', resp);
+    $scope.ingredient = resp.data;
+    
+  }, function(err) {
+    console.error('ERR', err);
+  })
+     $scope.getNutrientUnitType = function(id) {
+        // console.log(id);
+        return nutrientList.getNutrient(id).unitType;
+    };
+})
+
+.factory('nutrientList', function() {
+    var nutrients = {};
+
+    return {
+        setNutrients: function(_nutrients) {
+            for(var i=0; i<_nutrients.length; i++) {
+                var _nutrient = _nutrients[i];
+                nutrients[_nutrient.id] = _nutrient;
+            }
+        },
+        getNutrients: function() {
+            return nutrients;
+        },
+        getNutrient: function(id) {
+            return nutrients[id];
+        }
+    }
+})
+
+.run(function(nutrientList, $http, baseurl) {
+
+    $http.get(baseurl + '/nutrix-app/ws/nutrix/adm/nutrient', { 
+        params: {
+            offset: 0, max: 1000
+        }
+    })
+    .success(function(data) {
+        console.log(data);
+        nutrientList.setNutrients(data.collection);
+    });
+});
